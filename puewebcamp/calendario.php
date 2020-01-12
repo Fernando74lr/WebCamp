dc5c1d4d3f8a3803f24f9676d2c15a805f5e656d
Fernando74lr <flopzramirez@hotmail.com>
2020-01-11 22:34:37 -0600

    Creando archivo para sección de calendario

---
+++ puewebcamp/calendario.php
@@ -0,0 +1,8 @@
+<?php include_once 'includes/templates/header.php'; ?>
+
+  <section class="section contenedor">
+    <h2>Calendario de Eventos</h2>
+    
+  </section>
+
+<?php include_once 'includes/templates/footer.php'; ?>
