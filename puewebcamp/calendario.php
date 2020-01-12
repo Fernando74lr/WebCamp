c868a29758d38bbf6f2f755d9ea91caeb65e591a
Fernando74lr <flopzramirez@hotmail.com>
2020-01-11 23:59:10 -0600

    Consultando la base de datos con los eventos

--- puewebcamp/calendario.php
+++ puewebcamp/calendario.php
@@ -3,6 +3,52 @@
   <section class="section contenedor">
     <h2>Calendario de Eventos</h2>
     
+    <?php
+      /* Todo el código que escribas de SQL puede ser puesto 
+        dentro del PHP, MySql va a hacer que las consultas funcionen
+        y PHP se encarga de imprimir los resultados.
+      */
+
+      /*
+        Pasos para hacer una consulta a la Base de Datos.
+
+        1.- ABRIR conexión a la Base de Datos.
+        2.- Escribir la consulta que deseas hacer.
+        3.- Realizar la consulta.
+        4.- Imprimir resultados con la función fetch_assoc().
+        5.- CERRAR la conexión a la Base de Datos.
+            |
+            |--> ${NombreVariableConexión}->close();
+      */
+      try {
+        // require_once(), al igual que include_once(), es otra forma de incluir un archivo.
+        // 1.- Aquí se abre la conexión a la Base de Datos.
+        require_once('includes/funciones/bd_conexion.php');
+        // 2.- Consulta a la Base de Datos.
+        $sql = "SELECT * FROM eventos";
+        $resultado = $conn->query($sql);
+      } catch (\Exception $e) {
+        echo $e->getMessage();
+      }
+    ?>
+
+    <div class="calendario">
+      <?php
+      // 4.- Imprimir resultados.
+        $eventos = $resultado->fetch_assoc();
+      ?>
+      <pre>
+        <?php
+          var_dump($eventos);
+        ?>
+      </pre>
+    </div>
+
+    <?php
+      // 5.- Aquí se cierra la conexión a la Base de Datos.
+      $conn->close();
+    ?>
+
   </section>
 
 <?php include_once 'includes/templates/footer.php'; ?>
--- puewebcamp/includes/funciones/bd_conexion.php
+++ puewebcamp/includes/funciones/bd_conexion.php
@@ -1,6 +1,6 @@
 <?php
     // conn - conexión
-    $conn = new mysqli('localhost', 'root', '', 'puewebcamp');
+    $conn = new mysqli('localhost', 'root', '123456', 'puewebcamp');
 
     if ($conn->connect_error) {
         echo $error -> $conn->connect_error;
