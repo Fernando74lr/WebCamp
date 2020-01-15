604357b496156340c4f68eca920932e6f0ec9fed
Fernando74lr <flopzramirez@hotmail.com>
2020-01-15 16:08:38 -0600

    Se finalizó la sección de calendario de eventos

--- puewebcamp/calendario.php
+++ puewebcamp/calendario.php
@@ -25,7 +25,7 @@
         // 1.- Aquí se abre la conexión a la Base de Datos.
         require_once('includes/funciones/bd_conexion.php');
         // 2.- Consulta a la Base de Datos.
-        $sql = "SELECT evento_id, nombre_evento, fecha_evento, hora_evento, cat_evento, nombre_invitado, apellido_invitado ";
+        $sql = "SELECT evento_id, nombre_evento, fecha_evento, hora_evento, cat_evento, icono, nombre_invitado, apellido_invitado ";
         $sql .= " FROM eventos ";
         $sql .= " INNER JOIN categoria_evento ";
         $sql .= " ON eventos.id_cat_evento = categoria_evento.id_categoria ";
@@ -53,15 +53,52 @@
             'fecha' => $eventos['fecha_evento'],
             'hora' => $eventos['hora_evento'],
             'categoria' => $eventos['cat_evento'],
-            'eventos' => $eventos['nombre_invitado'] . ' ' . $eventos['apellido_invitado']
+            'icono' => $eventos['icono'],
+            'invitado' => $eventos['nombre_invitado'] . ' ' . $eventos['apellido_invitado']
           );
           $calendario[$fecha][] = $evento;
           ?>
-       <?php } ?>
+       <?php }  // Fin while ?>
 
-       <pre>
-          <?php var_dump($calendario); ?>
-        </pre>
+      <?php
+        // Imprime todos los eventos.
+        foreach($calendario as $dia => $lista_eventos) { ?>
+          <h3>
+            <i class="fa fa-calendar"></i>
+            <?php
+              // Unix 
+              setlocale(LC_TIME, 'es_ES.UTF-8');
+              // Windows
+              setlocale(LC_TIME, 'spanish');
+
+              echo strftime(  "%d de %B del %Y", strtotime($dia) );
+            ?>
+          </h3>
+          <?php foreach($lista_eventos as $evento) { ?>
+            <div class="dia">
+              <!-- Título -->
+              <p class="titulo"><?php echo $evento['titulo']; ?></p>
+
+              <!-- Hora -->
+              <p class="hora">
+                <i class="far fa-clock" aria-hidden="true"></i>
+                <?php echo $evento['fecha'] . " " . $evento['hora']; ?>
+              </p>
+
+              <!-- Categoría -->
+              <p>
+                <i class="fa <?php echo $evento['icono']; ?>" aria-hidden="true"></i>
+                <?php echo $evento['categoria']; ?>
+              </p>
+              
+              <!-- Invitado -->
+              <p>
+                <i class="fa fa-user" aria-hidden="true"></i>
+                <?php echo $evento['invitado']; ?>
+              </p>
+            </div>
+          <?php } //Fin foreach eventos ?>
+        <?php } // Fin foreach dias ?>
     </div>
 
     <?php
