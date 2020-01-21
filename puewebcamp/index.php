3e8a7a357f54debc7912f7587084fb673aea1f32
Fernando74lr <flopzramirez@hotmail.com>
2020-01-20 19:45:13 -0600

    Consulta para la página principal

--- puewebcamp/index.php
+++ puewebcamp/index.php
@@ -40,6 +40,29 @@
             <?php } ?>
           </nav><!--.menu-programa-->
 
+          <?php
+            try {
+              require_once('includes/funciones/bd_conexion.php');
+              $sql = "SELECT evento_id, nombre_evento, fecha_evento, hora_evento, cat_evento, icono, nombre_invitado, apellido_invitado ";
+              $sql .= " FROM eventos ";
+              $sql .= " INNER JOIN categoria_evento ";
+              $sql .= " ON eventos.id_cat_evento = categoria_evento.id_categoria ";
+              $sql .= " INNER JOIN invitados ";
+              $sql .= " ON eventos.id_inv = invitados.invitado_id ";
+              $sql .= " AND eventos.id_cat_evento = 1 ";
+              $sql .= " ORDER BY evento_id LIMIT 2 ";
+              $resultado = $conn->query($sql);
+            } catch (\Exception $e) {
+              echo $e->getMessage();
+            }
+          ?>
+
+          <?php $eventos = $resultado->fetch_assoc(); ?>
+
+          <pre>
+            <?php var_dump($eventos); ?>
+          </pre>
+
           <div id="talleres" class="info-curso ocultar clearfix ">
             <div class="detalle-evento">
               <h3>HTML5, CSS3, JavaScript</h3>
