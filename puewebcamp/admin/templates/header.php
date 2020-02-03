fe22972633f54a9ae7577bd920c081db6c316b71
Fernando74lr <flopzramirez@hotmail.com>
2020-02-03 16:30:22 -0600

    Creando el menú principal de Admin-LTE

--- puewebcamp/admin/templates/header.php
+++ puewebcamp/admin/templates/header.php
@@ -7,6 +7,8 @@
   <!-- Tell the browser to be responsive to screen width -->
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
+  <!-- admin -->
+  <link rel="stylesheet" href="css/admin.css">
   <!-- Font Awesome -->
   <link rel="stylesheet" href="css/all.min.css">
   <!-- Ionicons -->
