8ae58354bd4b7b36a3245b6b8d8c623dc25e1bec
Fernando74lr <flopzramirez@hotmail.com>
2020-02-11 23:45:39 -0600

    Archivos en el servidor y su nombre en la BD

--- puewebcamp/admin/modelo-invitado.php
+++ puewebcamp/admin/modelo-invitado.php
@@ -1,25 +1,54 @@
 <?php
     include_once 'funciones/funciones.php';
-    $nombre_categoria = $_POST['nombre_categoria'];
-    $icono = $_POST['icono'];
-    $id_registro = $_POST['id_registro'];
+    $nombre = $_POST['nombre_invitado'];
+    $apellido = $_POST['apellido_invitado'];
+    $biografia = $_POST['biografia_invitado'];
 
     if ($_POST['registro'] == 'nuevo') {
-        $respuesta = array(
+        /*$respuesta = array(
             'post' => $_POST,
             'file' => $_FILES
         );
         die(json_encode($respuesta));
+        */
 
+        $directorio = "../img/invitados/"; // Importante poner una '/' al final.
+
+        if (!is_dir($directorio)) { // is_dir() verifica que un directorio exista.
+            // Dirección para crear el directorio nuevo, 0755 es un permiso y true es que será recursivo.
+            // Que sea recursivo significa que le dará los permisos de ahora a todos los archivos nuevos, sino, tendríamos que asignarlos uno por uno
+            mkdir($directorio, 0755, true); // crea un directorio.
+        }
+
+        // RECOMENDACIÓN: ALMACENAR ARCHIVOS EN SERVIDOR Y SÓLO NOMBRES DE ARCHIVOS EN BASE DE DATOS.
+        // move_uploaded_file($path, $new_path) Mueve los archivos desde una ubicación temporal a la ubicación final.
+        /*
+            file:
+                archivo_imagen:
+                    * name: "2.jpg"
+                    type: "image/jpeg"
+                    * tmp_name: "C:\Bitnami\wampstack-7.2.26-0\php\tmp\phpB025.tmp"
+                    [...]
+
+        */
+        if (move_uploaded_file($_FILES['archivo_imagen']['tmp_name'], $directorio . $_FILES['archivo_imagen']['name'])) {
+            $imagen_url = $_FILES['archivo_imagen']['name'];
+            $imagen_resultado = "Se subió correctamente";
+        } else {
+            $respuesta = array(
+                'respuesta' => error_get_last() // Obtiene el último error registrado al intentar subir el archivo.
+            );
+        }
         try {
-            $stmt = $conn->prepare("INSERT INTO categoria_evento (cat_evento, icono) VALUES (?,?)");
-            $stmt->bind_param('ss', $nombre_categoria, $icono);
+            $stmt = $conn->prepare("INSERT INTO invitados (nombre_invitado, apellido_invitado, descripcion, url_imagen) VALUES (?,?,?,?)");
+            $stmt->bind_param('ssss', $nombre, $apellido, $biografia, $imagen_url);
             $stmt->execute();
             $id_insertado = $stmt->insert_id;
             if ($stmt->affected_rows > 0) {
                 $respuesta = array(
                     'respuesta' => 'exito',
-                    'id_insertado' => $id_insertado
+                    'id_insertado' => $id_insertado,
+                    'resultado_imagen' => $imagen_resultado
                 );
             } else {
                 $respuesta = array(
